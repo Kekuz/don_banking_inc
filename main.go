2f14db3f1d91c1358b4575ff2d1c6086d980aa88
Kekuz <anton.edlenko@yandex.ru>
2025-04-01 05:55:42 +0300

    add csv reader func

--- main.go
+++ main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
+	"log"
+	"os"
+
 	"github.com/Kekuz/don_banking_inc/domain"
 )
 
 func main() {
 	fmt.Println("This is Don banking APP!")
 	fmt.Println()
-	fmt.Println(makeAntonClient())
+	//fmt.Println(makeAntonClient())
+	records := readCsvFile("./client_mock_data.csv")
+    fmt.Println(records)
 }
 
 func makeAntonClient() domain.Client {
@@ -27,4 +33,20 @@ func makeAntonClient() domain.Client {
 			},
 		},
 	}
+}
+
+func readCsvFile(filePath string) [][]string {
+    f, err := os.Open(filePath)
+    if err != nil {
+        log.Fatal("Unable to read input file " + filePath, err)
+    }
+    defer f.Close()
+
+    csvReader := csv.NewReader(f)
+    records, err := csvReader.ReadAll()
+    if err != nil {
+        log.Fatal("Unable to parse file as CSV for " + filePath, err)
+    }
+
+    return records
 }
