5ba6b79edc98af6e0050669a1ba85cc92ecbf659
Kekuz <anton.edlenko@yandex.ru>
2025-04-17 00:08:15 +0300

    make console clear helper

--- cmd/main.go
+++ cmd/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	//"github.com/Kekuz/don_banking_inc/internal/service"
 	"github.com/Kekuz/don_banking_inc/internal/storage/csv"
 	"github.com/Kekuz/don_banking_inc/internal/transport/cli"
+	"github.com/Kekuz/don_banking_inc/pkg"
 )
 
 func main() {
@@ -24,6 +26,10 @@ func main() {
 		fmt.Printf("Данные для id %s: ", id)
 
 		fmt.Println(accountHandler.GetAccountsById(id))
+
+		time.Sleep(2 * time.Second)
+
+		pkg.CallClear()
 	}
 
 }
---
+++ pkg/consoleHelper.go
@@ -0,0 +1,33 @@
+package pkg
+
+import (
+    "os"
+    "os/exec"
+    "runtime"
+)
+
+var clear map[string]func() //create a map for storing clear funcs
+
+func init() {
+    clear = make(map[string]func())
+    clear["linux"] = func() { 
+        cmd := exec.Command("clear") //Linux
+        cmd.Stdout = os.Stdout
+        cmd.Run()
+    }
+
+    clear["windows"] = func() {
+        cmd := exec.Command("cmd", "/c", "cls") //Windows
+        cmd.Stdout = os.Stdout
+        cmd.Run()
+	}
+}
+
+func CallClear() {
+    value, ok := clear[runtime.GOOS] //runtime.GOOS -> linux, windows, darwin etc.
+    if ok {
+        value()
+    } else {
+        panic("Your platform is unsupported!")
+    }
+}
